d2038f892a9bc3e3b2e80d6a7bb9e4663415e698
Ulbandus <tovarishbozhanov@gmail.com>
2021-04-13 21:13:34 +0300

    Make group find page and small fixes, !Based on old DataBase type!
    Log:
    	modified:   app/routes.py
    	new file:   app/static/styles/group_finder.css
    	new file:   app/templates/group_finder/group_finder.html
    	modified:   app/tools.py
    	modified:   database/files.db

--- app/routes.py
+++ app/routes.py
@@ -2,19 +2,15 @@
 
 from os.path import join as join_path
 
-from flask import redirect, render_template, send_file
+from flask import redirect, render_template, send_file, request as flask_request
 
 from app import app
 
-from .tools import Files, Pages
+from .tools import DataBase
 
-pages = Pages()
-files = Files()
-
-
-@app.route('/error')
-def error():
-    return render_template(pages.get_page('error'))
+pages = [DataBase(), ['html', 'pages']]
+files = [DataBase(), ['path', 'files']]
+groups = [DataBase(), ['name, description', 'groups']]
 
 
 @app.route('/redirect/<page>', methods=['POST', 'GET'])
@@ -25,12 +21,28 @@ def redirect(page):
 @app.route('/favicon.ico')
 def favicon():
     path = join_path('/', app.root_path, 'static'
-                     ).replace('\\', '/') + files.get_file('favicon')
+                     ).replace('\\', '/') + pages[0].get(*pages[1], 'error')
     return send_file(path,
-                     files.get_file('favicon'))
+                     files[0].get(*files[1], 'favicon'))
 
 
-@app.errorhandler(Exception)
+@app.route('/find_groups')
+def group_finder():
+    result = []
+    search_text = flask_request.args.get("search", default='')
+    if search_text != '':
+        result = groups[0].get(*groups[1], search_text, False)
+        result = [[group[0], group[1]] for group in result]
+    print(result)
+    return render_template(
+        pages[0].get(*pages[1], 'group_finder'),
+        search_text=search_text,
+        result=result
+    )
+
+
+@ app.errorhandler(Exception)
 def handle_exception(error):
-    return render_template(pages.get_page('error'), e=error), 404
+    return render_template(pages[0].get(*pages[1], 'error'), e=error), 404
+
 
--- app/tools.py
+++ app/tools.py
@@ -5,7 +5,7 @@ from sqlite3 import connect
 
 
 class DataBase:
-    def __init__(self, path):
+    def __init__(self, path='./database/files.db'):
         self.path: str = path
 
     def execute(self, command: str) -> str:
@@ -14,14 +14,12 @@ class DataBase:
             result: str = cur.execute(command).fetchall()
         return result
 
-
-class Pages(DataBase):
-    def __init__(self):
-        super().__init__('./database/files.db')
-
-    def get_page(self, name: str) -> str:
-        return self.execute(
-            f'SELECT html FROM pages WHERE name = "{name}"')[0][0]
+    def get(self, value_key, page, value, one_result=True):
+        result = self.execute(
+            f'SELECT {value_key} FROM {page} WHERE name = "{value}"')
+        if one_result:
+            result = result[0][0]
+        return result
 
 
 class Files(DataBase):
@@ -33,3 +31,10 @@ class Files(DataBase):
             f'SELECT path FROM files WHERE name = "{name}"')[0][0]
 
 
+class Groups(DataBase):
+    def __init__(self):
+        super().__init__('./database/files.db')
+
+    def get_groups(self, name: str) -> str:
+        return self.execute(
+            f'SELECT path FROM files WHERE name = "{name}"')[0][0]
