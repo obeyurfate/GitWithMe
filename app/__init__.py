9e706974aaca55cbf7abcf8ab7d7d8d0c6a85dc9
obeyurfate <59767159+obeyurfate@users.noreply.github.com>
2021-04-20 14:50:15 +0300

    FIX problem after merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from os import urandom
+from os import getenv, urandom, environ
 
 from database import db_sess
 from dotenv import load_dotenv
@@ -10,14 +10,12 @@ from flask import Flask
 app = Flask(__name__, template_folder='./templates')
 load_dotenv('.env')
 
-database = "database/db.db"
-client_id = "9ff8f300ec3d64d48796"
-client_secret = "9e023423bc80ab40892da67823c24463487e7293"
-secret_key = "QWRWLIWDTDBDEFJACZEBYQEBQPUPZVVHUNSN"
+client_id = environ.get('client_id')
+client_secret = environ.get('client_secret')
 authorization_base_url = 'https://github.com/login/oauth/authorize'
 token_url = 'https://github.com/login/oauth/access_token'
 
 from app import routes
 
 app.secret_key = urandom(24)
-db_sess.global_init('database/db.db')
+db_sess.global_init(getenv("database"))
