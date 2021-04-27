31a0fd0a243b73ff3545cefeba6ca34c0808251f
obeyurfate <59767159+obeyurfate@users.noreply.github.com>
2021-04-27 20:24:54 +0300

    Hotfix

--- app/routes.py
+++ app/routes.py
@@ -314,7 +314,7 @@ def ide():
 def delete_group():
     current_sess = db_sess.create_session()
     if request.method == 'POST':
-        group = current_sess.query(Groups).filter(Groups.name == request.form['groupSelect']).filter()
+        group = current_sess.query(Groups).filter(Groups.name == request.form['groupSelect']).first()
         if group:
             current_sess.delete(group)
         return redirect('/find_groups')
